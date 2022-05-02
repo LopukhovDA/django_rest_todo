e08bfc3c7b45a58ce2ef38994738e31448980812
Dmitriy <lopukhovda@gmail.com>
2022-05-02 23:55:49 +0300

    Homework8
    Домашняя работа к 8-му уроку

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
-from .serializers import ProjectModelSerializer, ToDoModelSerializer
+from .serializers import ProjectModelSerializer, ToDoModelSerializer, ToDoModelSerializerBase
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.pagination import LimitOffsetPagination
 from .filters import ProjectFilter
@@ -20,7 +20,7 @@ class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
-    permission_classes = [permissions.IsAuthenticated]
+#    permission_classes = [permissions.IsAuthenticated]
 #    pagination_class = ProjectLimitOffsetPagination
 #    filterset_class = ProjectFilter
 
@@ -29,9 +29,14 @@ class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
-    permission_classes = [permissions.IsAuthenticated]
+#    permission_classes = [permissions.IsAuthenticated]
 #    pagination_class = ToDoLimitOffsetPagination
 #    filterset_fields = ['project']
+
+    def get_serializer_class(self):
+        if self.request.method in ['GET']:
+            return ToDoModelSerializer
+        return ToDoModelSerializerBase
 
     def perform_destroy(self, instance):
         instance.is_active = False
